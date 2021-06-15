614439a729a91e6393cc2bb370e1dd26d234f4ea
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-14 22:45:08 -0600

    add global state object refactoring

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 
 import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
+import spinner from '../assets/spinner.gif';
 
 function Detail() {
+  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({})
+  const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  const { products } = state;
+
 
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
-    }
-  }, [products, id]);
-
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+    } //make sure that we have all the products in the global state object, or first time users wont be able to see individual product data pages ---- dependency array
+  }, [products, data, dispatch, id]);
+  
   return (
     <>
       {currentProduct ? (

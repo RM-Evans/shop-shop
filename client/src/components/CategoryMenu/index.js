fdead8f18c827d21e04ebe115c6a79fae04376be
Jawsbreaker <evansrichardmichael@gmail.com>
2021-06-17 17:28:35 -0600

    implemented IndexedDB offline capabilities (PWA)

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -3,18 +3,19 @@ import { useQuery } from '@apollo/react-hooks';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
 
 
 
 
-//retrieve current global state and dispatch method to update state
+  //retrieve current global state and dispatch method to update state
   const [state, dispatch] = useStoreContext();
-//only using categories array from the globalstate -- destructure
+  //only using categories array from the globalstate -- destructure
   const { categories } = state;
-
-  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  //note that we want to destructure the loading variable for use with indexedDB
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     // if categoryData exists or has changed from the response of useQuery, then run dispatch()
@@ -23,8 +24,19 @@ function CategoryMenu() {
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
+      //write to indexedDB categories object store 
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
     }
-  }, [categoryData, dispatch]);
+  }, [categoryData, loading, dispatch]);
 
   const handleClick = id => {
     // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
